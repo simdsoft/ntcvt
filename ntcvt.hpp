35a8a883e455c9e7bd47d8694245d2894a3bf41e
halx99 <halx99@live.com>
2020-07-16 23:12:52 +0800

    Remove unnecessary

--- ntcvt.hpp
+++ ntcvt.hpp
@@ -2,16 +2,14 @@
 #define SIMDSOFT__NTCVT_HPP
 
 #pragma once
+#if !defined(WIN32_LEAN_AND_MEAN)
+#  define WIN32_LEAN_AND_MEAN
+#endif
 #include <Windows.h>
 #include <string>
 
 namespace ntcvt
 {
-enum code_page
-{
-  code_page_acp  = CP_ACP,
-  code_page_utf8 = CP_UTF8
-};
 
 // different with resize, not all of fill new memory with '\0'
 namespace buffer_traits
@@ -69,7 +67,7 @@ template <typename _Elem> _Elem* inplaced(CStringT<_Elem, StrTraitMFC_DLL<_Elem>
 } // namespace buffer_traits
 
 template <typename _StringContType>
-inline _StringContType wcbs2a(const wchar_t* wcb, int len, UINT cp = code_page_acp)
+inline _StringContType wcbs2a(const wchar_t* wcb, int len, UINT cp = CP_ACP)
 {
   if (len == -1)
     len = lstrlenW(wcb);
@@ -81,7 +79,7 @@ inline _StringContType wcbs2a(const wchar_t* wcb, int len, UINT cp = code_page_a
 }
 
 template <typename _StringContType>
-inline _StringContType mcbs2w(const char* mcb, int len, UINT cp = code_page_acp)
+inline _StringContType mcbs2w(const char* mcb, int len, UINT cp = CP_ACP)
 {
   if (len == -1)
     len = lstrlenA(mcb);
@@ -93,34 +91,60 @@ inline _StringContType mcbs2w(const char* mcb, int len, UINT cp = code_page_acp)
   return buffer;
 }
 
+inline int mcbs2w(const char* mcb, int len, wchar_t* wbuf, int wbuf_len, UINT cp = CP_ACP)
+{
+  if (len == -1)
+    len = lstrlenA(mcb);
+  int cch;
+  if (len > 0 && (cch = ::MultiByteToWideChar(cp, 0, mcb, len, NULL, 0)) > 0)
+    return ::MultiByteToWideChar(cp, 0, mcb, len, wbuf, wbuf_len);
+
+  return 0;
+}
+
+inline wchar_t* mcbs2wdup(const char* mcb, int len, int* wbuf_len, UINT cp = CP_ACP)
+{
+  if (len == -1)
+    len = lstrlenA(mcb);
+  int cch;
+  if (len > 0 && (cch = ::MultiByteToWideChar(cp, 0, mcb, len, NULL, 0)) > 0)
+  {
+    wchar_t* wbuf = (wchar_t*)calloc(cch + 1, 2);
+    *wbuf_len     = ::MultiByteToWideChar(cp, 0, mcb, len, wbuf, cch);
+    return wbuf;
+  }
+
+  return (wchar_t*)calloc(1, 2);
+}
+
 #if _HAS_CXX17
-inline std::string from_chars(const std::wstring_view& wcb, UINT cp = code_page_acp)
+inline std::string from_chars(const std::wstring_view& wcb, UINT cp = CP_ACP)
 {
   return wcbs2a<std::string>(wcb.data(), wcb.length(), cp);
 }
 
-inline std::wstring from_chars(const std::string_view& mcb, UINT cp = code_page_acp)
+inline std::wstring from_chars(const std::string_view& mcb, UINT cp = CP_ACP)
 {
   return mcbs2w<std::wstring>(mcb.data(), mcb.length(), cp);
 }
 #else
-inline std::string from_chars(const std::wstring& wcb, UINT cp = code_page_acp)
+inline std::string from_chars(const std::wstring& wcb, UINT cp = CP_ACP)
 {
   return wcbs2a<std::string>(wcb.c_str(), wcb.length(), cp);
 }
 
-inline std::wstring from_chars(const std::string& mcb, UINT cp = code_page_acp)
+inline std::wstring from_chars(const std::string& mcb, UINT cp = CP_ACP)
 {
   return mcbs2w<std::wstring>(mcb.c_str(), mcb.length(), cp);
 }
 #endif
 
-inline std::string from_chars(const wchar_t* str, UINT cp = code_page_acp)
+inline std::string from_chars(const wchar_t* str, UINT cp = CP_ACP)
 {
   return wcbs2a<std::string>(str, -1, cp);
 }
 
-inline std::wstring from_chars(const char* str, UINT cp = code_page_acp)
+inline std::wstring from_chars(const char* str, UINT cp = CP_ACP)
 {
   return mcbs2w<std::wstring>(str, -1, cp);
 }
@@ -139,7 +163,7 @@ inline CStringW from_chars(std::string_view mcb, UINT cp = CP_ACP)
   return mcbs2w<CStringW>(mcb.data(), mcb.length(), cp);
 }
 #  else
-inline CStringW from_chars(const char* str, UINT cp = code_page_acp)
+inline CStringW from_chars(const char* str, UINT cp = CP_ACP)
 {
   return mcbs2w<CStringW>(str, -1, cp);
 }
