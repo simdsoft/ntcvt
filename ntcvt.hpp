647e8b413290a03cff846be13611781ef1b6a5a1
halx99 <halx99@live.com>
2023-02-04 14:30:20 +0800

    Fix warnings

--- ntcvt.hpp
+++ ntcvt.hpp
@@ -1,3 +1,4 @@
+// require C++17+
 #ifndef SIMDSOFT__NTCVT_HPP
 #define SIMDSOFT__NTCVT_HPP
 
@@ -40,7 +41,7 @@ public:
   // std::string: use memset (usually implemented with SIMD)
   // std::wstring: for loop (slow performance)
   // only works on msvc currently
-  _Elem* resize_nofill(int len)
+  _Elem* resize_for_overwrite(size_t len)
   {
     this->reserve(len);
 #if _MSC_VER >= 1920 // VS2019+
@@ -57,15 +58,21 @@ public:
   }
 };
 
-template <typename _Elem> static _Elem* prepare(std::basic_string<_Elem>& str, int size)
+template <typename _Cont> inline
+auto prepare(_Cont& str, size_t size)
 {
+  using _Elem = typename _Cont::value_type;
   intrusive_string<_Elem>& helper = (intrusive_string<_Elem>&)str;
-  return helper.resize_nofill(size);
+  return helper.resize_for_overwrite(size);
 }
 #if defined(_AFX)
-template <typename _Elem> _Elem* prepare(CStringT<_Elem, StrTraitMFC_DLL<_Elem>>& str, int size)
-{
-  return str.GetBufferSetLength(size);
+template <> inline
+auto prepare<CStringW>(CStringW& str, size_t size) {
+    return str.GetBufferSetLength(static_cast<int>(size));
+}
+template <> inline
+auto prepare<CStringA>(CStringA& str, size_t size) {
+    return str.GetBufferSetLength(static_cast<int>(size));
 }
 #endif
 } // namespace buffer_traits
@@ -124,22 +131,22 @@ inline wchar_t* mcbs2wdup(const char* mcb, int len, int* wbuf_len, UINT cp = NTC
 #if _HAS_CXX17
 inline std::string from_chars(const std::wstring_view& wcb, UINT cp = NTCVT_CP_DEFAULT)
 {
-  return wcbs2a<std::string>(wcb.data(), wcb.length(), cp);
+  return wcbs2a<std::string>(wcb.data(), static_cast<int>(wcb.length()), cp);
 }
 
 inline std::wstring from_chars(const std::string_view& mcb, UINT cp = NTCVT_CP_DEFAULT)
 {
-  return mcbs2w<std::wstring>(mcb.data(), mcb.length(), cp);
+  return mcbs2w<std::wstring>(mcb.data(), static_cast<int>(mcb.length()), cp);
 }
 #else
 inline std::string from_chars(const std::wstring& wcb, UINT cp = NTCVT_CP_DEFAULT)
 {
-  return wcbs2a<std::string>(wcb.c_str(), wcb.length(), cp);
+  return wcbs2a<std::string>(wcb.c_str(), static_cast<int>(wcb.length()), cp);
 }
 
 inline std::wstring from_chars(const std::string& mcb, UINT cp = NTCVT_CP_DEFAULT)
 {
-  return mcbs2w<std::wstring>(mcb.c_str(), mcb.length(), cp);
+  return mcbs2w<std::wstring>(mcb.c_str(), static_cast<int>(mcb.length()), cp);
 }
 #endif
 
@@ -164,7 +171,7 @@ namespace afx
 #  if _HAS_CXX17
 inline CStringW from_chars(std::string_view mcb, UINT cp = NTCVT_CP_DEFAULT)
 {
-  return mcbs2w<CStringW>(mcb.data(), mcb.length(), cp);
+  return mcbs2w<CStringW>(mcb.data(), static_cast<int>(mcb.length()), cp);
 }
 #  else
 inline CStringW from_chars(const char* str, UINT cp = NTCVT_CP_DEFAULT)
@@ -173,7 +180,7 @@ inline CStringW from_chars(const char* str, UINT cp = NTCVT_CP_DEFAULT)
 }
 inline CStringW from_chars(const std::string& mcb, UINT cp = NTCVT_CP_DEFAULT)
 {
-  return mcbs2w<CStringW>(mcb.c_str(), mcb.length(), cp);
+  return mcbs2w<CStringW>(mcb.c_str(), static_cast<int>(mcb.length()), cp);
 }
 #  endif
 } // namespace afx
